673f9c9ee5362ec16fb6b54bd97904f66841b1e5
Ash Wilson <ash.d.wilson@gmail.com>
2016-11-18 14:42:13 -0800

    Putting agent in read-only mode

--- runner.sh
+++ runner.sh
@@ -6,5 +6,5 @@ if [ -e /opt/cloudpassage/data/store.db ]
 then
   /opt/cloudpassage/bin/cphalo
 else
-  /opt/cloudpassage/bin/cphalo --agent-key=$HALO_AGENT_KEY --tag=$SERVER_TAG --server-label=$SERVER_LABEL
+  /opt/cloudpassage/bin/cphalo --agent-key=$HALO_AGENT_KEY --tag=$SERVER_TAG --server-label=$SERVER_LABEL --read-only=true
 fi
